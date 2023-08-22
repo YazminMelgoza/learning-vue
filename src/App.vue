f97f05d273800c4f1a3904b005b8d964a292347d
YazminMelgoza <87241610+YazminMelgoza@users.noreply.github.com>
2023-08-22 14:51:33 -0600

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref } from 'vue'
+
+const contador = ref(0)
+
+function incrementar() {
+  contador.value++
+}
+</script>
+
+<template>
+  <h2 :class="contador >= 20 ? 'red' : ''">Contador reactivo: {{ contador }}</h2>
+  <button @click="incrementar">Aumentar</button>
+  <button @click="incrementar()">Decrementar</button>
+</template>
+
+<style>
+.red {
+  color: red;
+}
+</style>
